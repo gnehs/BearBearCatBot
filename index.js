80b3926c6b84b16f1c0d8c65f9e6baaff1f596b1
棒棒勝 <16719720+gnehs@users.noreply.github.com>
2017-09-10 10:03:34 +0800

    好

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 // 載入
 var token = require('./token.js');
 var TelegramBot = require('node-telegram-bot-api');
-var bot = new TelegramBot(token.botToken, {polling: true});
+var bot = new TelegramBot(token.botToken, { polling: true });
 
 // 啟動成功
-bot.sendMessage('-1001127892867', '熊貓貓啟動成功！');
-console.log('熊貓貓啟動成功！')
+var start_time = new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds(); // 機器人啟動時間
+console.log("[系統]熊貓貓在 " + start_time + " 時啟動成功");
+bot.sendMessage('-1001127892867', "`[系統]`熊貓貓在 " + start_time + " 時啟動成功", { parse_mode: "markdown" });
+bot.sendMessage('-1001143743775', "`[系統]`熊貓貓在 " + start_time + " 時啟動成功", { parse_mode: "markdown" });
 
-bot.onText(/\/start/, function (msg) {
+// /start
+bot.onText(/\/start/, function(msg) {
     var chatId = msg.chat.id;
-    var resp = '哈囉！';
+    var resp = '哈囉！這裡是熊貓貓';
     bot.sendMessage(chatId, resp);
 });
 
+// 重複講話(HTML)
+bot.onText(/\/echo (.+)/, function(msg, match) {
+    var resp = match[1];
+    if (!match[1]) { var resp = "你沒傳訊息"; }
+    bot.sendMessage(msg.chat.id, resp, { parse_mode: "HTML", reply_to_message_id: msg.message_id });
+});
 
-bot.onText(/\/cal (.+)/, function (msg, match) {
-    var fromId = msg.from.id; //用戶的ID
-    var resp = match[1].replace(/[^-()\d/*+.]/g, '');
-    // match[1]的意思是 /cal 後面的所有內容
-    resp = '計算結果為: ' + eval(resp);
-    // eval是用作執行計算的function
-    bot.sendMessage(fromId, resp); //發送訊息的function
+
+//鍵盤新增跟移除
+bot.onText(/\/addKeyboard/, function(msg) {
+    const opts = {
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ['歡迎光臨貼圖群']
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: true,
+            reply_to_message_id: msg.message_id
+        })
+    };
+    bot.sendMessage(msg.chat.id, '鍵盤已新增', opts);
+});
+
+bot.onText(/\/removeKeyboard/, function(msg) {
+    const opts = {
+        reply_markup: JSON.stringify({
+            remove_keyboard: true,
+            reply_to_message_id: msg.message_id
+        })
+    };
+    bot.sendMessage(msg.chat.id, '鍵盤已移除', opts);
 });
